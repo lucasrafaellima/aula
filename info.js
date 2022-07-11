9a3f218808cd546598255cb3f58231a884844281
lucasrafaellima <87723096+lucasrafaellima@users.noreply.github.com>
2022-07-11 15:41:44 -0300

    added info

---
+++ info.js
@@ -0,0 +1,90 @@
+function mostra1() {
+    let sinalizar = document.getElementsByClassName("sinal")[0];
+    let sinalizar1 = document.getElementsByClassName("sinal1")[0];
+    let mostrar = document.getElementsByClassName("resposta")[0];
+    if (mostrar.style.display === 'none') {
+        mostrar.style.display = 'block';
+        sinalizar.style.display = 'none';
+        sinalizar1.style.display = 'block';
+    } else {
+        mostrar.style.display = 'none';
+        sinalizar.style.display = 'block';
+        sinalizar1.style.display = 'none';
+    }
+}
+
+function mostra2() {
+    let sinalizar = document.getElementsByClassName("sinal")[1];
+    let sinalizar1 = document.getElementsByClassName("sinal1")[1];
+    let mostrar = document.getElementsByClassName("resposta")[1];
+    if (mostrar.style.display === 'none') {
+        mostrar.style.display = 'block';
+        sinalizar.style.display = 'none';
+        sinalizar1.style.display = 'block';
+    } else {
+        mostrar.style.display = 'none';
+        sinalizar.style.display = 'block';
+        sinalizar1.style.display = 'none';
+    }
+}
+
+function mostra3() {
+    let sinalizar = document.getElementsByClassName("sinal")[2];
+    let sinalizar1 = document.getElementsByClassName("sinal1")[2];
+    let mostrar = document.getElementsByClassName("resposta")[2];
+    if (mostrar.style.display === 'none') {
+        mostrar.style.display = 'block';
+        sinalizar.style.display = 'none';
+        sinalizar1.style.display = 'block';
+    } else {
+        mostrar.style.display = 'none';
+        sinalizar.style.display = 'block';
+        sinalizar1.style.display = 'none';
+    }
+}
+
+function mostra4() {
+    let sinalizar = document.getElementsByClassName("sinal")[3];
+    let sinalizar1 = document.getElementsByClassName("sinal1")[3];
+    let mostrar = document.getElementsByClassName("resposta")[3];
+    if (mostrar.style.display === 'none') {
+        mostrar.style.display = 'block';
+        sinalizar.style.display = 'none';
+        sinalizar1.style.display = 'block';
+    } else {
+        mostrar.style.display = 'none';
+        sinalizar.style.display = 'block';
+        sinalizar1.style.display = 'none';
+    }
+}
+
+function mostra5() {
+    let sinalizar = document.getElementsByClassName("sinal")[4];
+    let sinalizar1 = document.getElementsByClassName("sinal1")[4];
+    let mostrar = document.getElementsByClassName("resposta")[4];
+    if (mostrar.style.display === 'none') {
+        mostrar.style.display = 'block';
+        sinalizar.style.display = 'none';
+        sinalizar1.style.display = 'block';
+    } else {
+        mostrar.style.display = 'none';
+        sinalizar.style.display = 'block';
+        sinalizar1.style.display = 'none';
+    }
+}
+
+function mostra6() {
+    let sinalizar = document.getElementsByClassName("sinal")[5];
+    let sinalizar1 = document.getElementsByClassName("sinal1")[5];
+    let mostrar = document.getElementsByClassName("resposta")[5];
+    if (mostrar.style.display === 'none') {
+        mostrar.style.display = 'block';
+        sinalizar.style.display = 'none';
+        sinalizar1.style.display = 'block';
+    } else {
+        mostrar.style.display = 'none';
+        sinalizar.style.display = 'block';
+        sinalizar1.style.display = 'none';
+    }
+}
+
