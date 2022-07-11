06793be764f8a74f9a11b95b9b183a60649ff042
lucasrafaellima <87723096+lucasrafaellima@users.noreply.github.com>
2022-07-11 08:18:12 -0300

    added library

---
+++ meu-app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Primeiro codigo em JSON
+        </p>
+          react styled components
+          Learn React
+      </header>
+    </div>
+  );
+}
+
+export default App;
